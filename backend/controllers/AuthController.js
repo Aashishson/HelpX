63ac8d1c89ac0a6ca5f2b5e5ad794ff921e7b0db
Aashishson <theaashishsoni@gmail.com>
2026-03-01 19:28:14 +0530

    Some improvements

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,90 +1,84 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const UserModel = require("../models/UserModel");
 const JWT_SECRET = process.env.JWT_SECRET;
-const generateAccessToken = require("../helpers/token");
-const generateRefreshToken = require("../helpers/token");
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../helpers/token");
 const cookies = require("cookie-parser");
 
-
-exports.Register = async(req , res) => {
-    let {Username,Email,Password} = req.body;
-    let checkUserExists = await UserModel.findOne({
-        Email: Email.toLowerCase()
-    })
-    if(checkUserExists){
-        res.status(400).json({
-            message: "User already Exists! Please try another Email."
-        })
-    }
-
-    let newUser = new UserModel({
-        Email: Email,
-        Password: await bcrypt.hash(Password , 12),
-        Username: Username,
-        Tokens:[{
-            Active : True,
-            type: "Email-verification",
-            token: verifyUserToken
-        }]
+exports.Register = async (req, res) => {
+  let { Username, Email, Password } = req.body;
+  let checkUserExists = await UserModel.findOne({
+    Email: Email.toLowerCase(),
+  });
+  if (checkUserExists) {
+    res.status(400).json({
+      message: "User already Exists! Please try another Email.",
     });
+  }
 
-    await Promise.all([newUser.save()]);
+  let newUser = await new UserModel({
+    Email: Email,
+    Password: await bcrypt.hash(Password, 12),
+    Username: Username,
+    Tokens: [
+      {
+        Active: True,
+        type: "Email-verification",
+        token: verifyUserToken,
+      },
+    ],
+  });
 
-    res.status(200).json({
-        message: "Account successfully registered!"
-    })
-}
+  await Promise.all([newUser.save()]);
 
-exports.LocalLogin = async(req,res) => {
+  res.status(200).json({
+    message: "Account successfully registered!",
+  });
+};
 
-    try{
-    let [UserEmail , Password] = req.body;
+exports.LocalLogin = async (req, res) => {
+  try {
+    let { UserEmail, Password } = req.body;
     UserEmail = UserEmail.toLowerCase();
-    let User = UserModel.findOne({UserEmail});
-    if(!User){
-        res.status(404).json({
-            message: "User Not Found!"
-        })
+    let User = await UserModel.findOne({ UserEmail });
+    if (!User) {
+      res.status(404).json({
+        message: "User Not Found!",
+      });
     }
 
-    let checkPassword = bcrypt.compare(Password,User.Password);
+    let checkPassword = await bcrypt.compare(Password, User.Password);
 
-    if(!checkPassword){
-        res.status(400).json({
-            message: "Invalid Credentials!"
-        })
+    if (!checkPassword) {
+      res.status(400).json({
+        message: "Invalid Credentials!",
+      });
     }
 
-    //Generate Tokens: 
+    //Generate Tokens:
     const accessToken = generateAccessToken(User);
     const refreshToken = generateRefreshToken(User);
-  
-    req.cookies("nd_Refresh" , refreshToken,{
-        samesite: none ,
-        secure: process.env.NODE_ENV === "production",
-        httpOnly: true,
-        success: true,
-        expire: new Date(Date.now() + 7*24*60*60*1000)
-    }
-    )
+
+    req.cookie("nd_Refresh", refreshToken, {
+      samesite: "none",
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    });
 
     const resObj = {
-        status:201,
-        accessToken,
-        role: User.role
-    }
+      status: 201,
+      accessToken,
+      role: User.role,
+    };
 
     res.json(resObj);
-
-}catch(error){
+  } catch (error) {
     res.status(500).json({
-        message: "Internal Server Error!"
-    })
-}
-
-
-    
-
-
-}
+      message: "Internal Server Error!",
+    });
+  }
+};
